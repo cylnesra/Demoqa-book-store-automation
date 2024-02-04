f3eb022524831efd4717d5c95e2992ffe5413fad
Esra Ceylan <87074889+cylnesra@users.noreply.github.com>
2024-02-04 11:12:49 +0300

    Update TestResultLogger.java

--- demoqa-book-automation/src/test/java/base/TestResultLogger.java
+++ demoqa-book-automation/src/test/java/base/TestResultLogger.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.extension.TestWatcher;
 import java.util.Optional;
 
 public class TestResultLogger implements TestWatcher {
-    //The TestWatcher API was used to monitor executed tests and for logging operations.
-    //The TestWatcher API is an interface. Once we implemented it,
-    //we overridden its methods and populated them with the actions we needed to perform.
+
     Log log = new Log();
     @Override
     public void testDisabled(ExtensionContext context, Optional<String> reason) {
